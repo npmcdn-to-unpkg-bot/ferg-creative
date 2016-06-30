61bcb44bd9b73ee1da11cca913f824640c09300b
mescolito <Sean@b-mdrinkwater.nativesun.local>
2016-06-30 12:24:26 -0400

    Added Grid experiment
    adding angular js to samsung grid project

---
+++ grid-experiment/js/controllers/MainController.js
@@ -0,0 +1,38 @@
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.title = 'Poop on a stick';
+  $scope.promo = "Less Push more shove";
+  $scope.products = [
+  {
+    name: 'The Book of Trees',
+    cover: 'img/the-book-of-trees.jpg'
+  },
+  {
+    name: 'Program or be Programmed',
+    cover: 'img/program-or-be-programmed.jpg'
+  },
+  {
+    name: 'The Book of Trees',
+    cover: 'img/the-book-of-trees.jpg'
+  },
+  {
+    name: 'Program or be Programmed',
+    cover: 'img/program-or-be-programmed.jpg'
+  },
+  {
+    name: 'The Book of Trees',
+    cover: 'img/the-book-of-trees.jpg'
+  },
+  {
+    name: 'Program or be Programmed',
+    cover: 'img/program-or-be-programmed.jpg'
+  },
+  {
+    name: 'The Book of Trees',
+    cover: 'img/the-book-of-trees.jpg'
+  },
+  {
+    name: 'Program or be Programmed',
+    cover: 'img/program-or-be-programmed.jpg'
+  }
+];
+}]);
